25f5ac2671fbcef0bbd9dd579023e0d8238340c7
Vijaya Sharma <sharmavijaya997@gmail.com>
2024-05-29 13:31:15 +0545

    Que3

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from django import views
 from django.urls import path
 from .views import (
     PostListView,
@@ -10,5 +11,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
     path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('add-blog/',views.AddBlog.as_view(), name='add_blog'),
     # Add other URL patterns for your blog app here
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,36 @@
-from multiprocessing import AuthenticationError
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.contrib.auth.forms import AuthenticationForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, LoginView, DeleteView
+from .forms import PostForm  # Assuming you have a PostForm defined in forms.py
 from .models import Post
-from django.views import View
-from django.views.generic import LoginView
 
-# Create your views here.
 class PostList(ListView):
-    model=Post
-    context_object_name='list'
-    template_name="registration/blog.html"
-class LoginView(LoginView):
-    form_class=AuthenticationError
-    authentication_form=None
-    template_name="registration/login.html"
-    redirect_authenticated_user=False
-    extra_content=None
-class Addlog(CreateView):
-    model=Post
-    form_class=PostForm
-    template_name='registration/login.html'
+    model = Post
+    context_object_name = 'list'
+    template_name = "registration/blog.html"
+
+class CustomLoginView(LoginView):
+    form_class = AuthenticationForm
+    authentication_form = None
+    template_name = "registration/login.html"
+    redirect_authenticated_user = False
+    extra_content = None
+
+class AddBlog(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'registration/add_blog.html'
+    success_url = reverse_lazy('blog')
+
+class UpdateBlog(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'registration/update_blog.html'
+    success_url = reverse_lazy('blog')
+
+class DeleteBlog(DeleteView):
+    model = Post
+    form_class = PostForm
+    template_name = 'registration/update_blog.html'
+    
+
