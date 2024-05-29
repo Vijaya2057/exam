6b44dbe17112553c44c47856807d1e3e2394868f
Vijaya Sharma <sharmavijaya997@gmail.com>
2024-05-29 13:13:33 +0545

    Que3

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,14 @@
-
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    # import other views as needed
+)
+
+app_name = 'blog'
 
 urlpatterns = [
-    
+    path('', PostListView.as_view(), name='post-list'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # Add other URL patterns for your blog app here
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
+from multiprocessing import AuthenticationError
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from .models import Post
+from django.views import View
+from django.views.generic import LoginView
 
 # Create your views here.
+class PostList(ListView):
+    model=Post
+    context_object_name='list'
+    template_name="registration/blog.html"
+class LoginView(LoginView):
+    form_class=AuthenticationError
+    authentication_form=None
+    template_name="registration/login.html"
+    redirect_authenticated_user=False
+    extra_content=None
+class Addlog(CreateView):
+    model=Post
+    form_class=PostForm
+    template_name='registration/login.html'
